51dbd8d90814f720b66b4e0339d14cc4c95d5c12
unknown <f.hahne@dkfz-heidelberg.de>
2009-07-09 23:04:28 +0000

    adding support for QA plots
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowStats@40583 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/warpSet.R
+++ R/warpSet.R
@@ -1,7 +1,8 @@
 ## Align data in a flowSet by estimating high density regions and using this
 ## information as landmarks. This works separately on each parameter.
 warpSet <- function(x, stains, grouping=NULL, monwrd=TRUE, subsample=NULL,
-                    peakNr=NULL, clipRange=0.01, nbreaks=11, fres, ...)
+                    peakNr=NULL, clipRange=0.01, nbreaks=11, fres, warpFuns=FALSE,
+                    ...)
 {
     ## Some type-checking first
     flowCore:::checkClass(x, "flowSet")
@@ -49,6 +50,7 @@ warpSet <- function(x, stains, grouping=NULL, monwrd=TRUE, subsample=NULL,
 
     ## iterate over stains
     eps <- .Machine$double.eps
+    wfuns <- list()
     for(p in stains)
     {
         ## set up fda parameters
@@ -87,9 +89,21 @@ warpSet <- function(x, stains, grouping=NULL, monwrd=TRUE, subsample=NULL,
                         ".\nWarping might have removed signal!")
         }
         ## fill NAs with column medians
+        regions <- attr(landmarks, "regions")
+        dists <- attr(landmarks, "cdists") 
+        attr(landmarks, c("regions", "cdists")) <- NULL
+        hasPeak <- !is.na(landmarks)
         for(n in 1:ncol(landmarks))
-            landmarks[is.na(landmarks[,n]),n] <-
-                mean(landmarks[,n], na.rm=TRUE)
+        {
+            nar <- is.na(landmarks[,n])
+            landmarks[nar,n] <- mean(landmarks[,n], na.rm=TRUE)
+            m <- matrix(apply(sapply(regions[!nar],
+                                     function(r) if(is.null(dim(r))) r else r[n,]),
+                              1, mean, na.rm=TRUE), ncol=2)
+            for(i in names(which(nar)))
+                regions[[i]][n,] <- m
+        }
+            
         ## register the densities
         if(ncol(landmarks)==1){ ## only one peak: offset
             offsets <- landmarks-median(landmarks)
@@ -109,48 +123,56 @@ warpSet <- function(x, stains, grouping=NULL, monwrd=TRUE, subsample=NULL,
             ## funs <-  apply(warpedX, 2, function(y) approxfun(argvals, y))
             funs <-  apply(warpedX, 2, approxfun, argvals)
         }
-        warpedLandmarks <- landmarks
-        leftBoard <- rightBoard <- list(length(funs))
-        newRange <- c(Inf, -Inf)
-        ## transform the raw data using the warping functions
-        for(i in seq_along(funs)){
-            thisDat <- exprs(expData[[i]][,p])
-            lb <- thisDat < ranges[[i]][1,p]+eps
-            lb[is.na(lb)] <- TRUE
-            leftBoard[[i]] <- lb
-            rb <- thisDat > ranges[[i]][2,p]-eps
-            rb[is.na(rb)] <- TRUE   
-            rightBoard[[i]] <- rb
-            sel <- leftBoard[[i]] | rightBoard[[i]]
-            newDat <- as.matrix(funs[[i]](thisDat[!sel,]))
-            newDat[is.na(newDat)] <- thisDat[!sel,][is.na(newDat)]
-            exprs(expData[[i]])[!sel,p] <- newDat
-            warpedLandmarks[i, ] <- funs[[i]](landmarks[i,])
-            newRange[1] <- min(newRange[1], min(exprs(expData[[i]])[,p], na.rm=TRUE))
-            newRange[2] <- max(newRange[2], max(exprs(expData[[i]])[,p], na.rm=TRUE))
+        names(funs) <- sampleNames(x)
+        wfuns[[p]] <- funs
+        if(!warpFuns)
+        {
+            warpedLandmarks <- landmarks
+            leftBoard <- rightBoard <- list(length(funs))
+            newRange <- c(Inf, -Inf)
+            ## transform the raw data using the warping functions
+            for(i in seq_along(funs)){
+                thisDat <- exprs(expData[[i]][,p])
+                lb <- thisDat < ranges[[i]][1,p]+eps
+                lb[is.na(lb)] <- TRUE
+                leftBoard[[i]] <- lb
+                rb <- thisDat > ranges[[i]][2,p]-eps
+                rb[is.na(rb)] <- TRUE   
+                rightBoard[[i]] <- rb
+                sel <- leftBoard[[i]] | rightBoard[[i]]
+                newDat <- as.matrix(funs[[i]](thisDat[!sel,]))
+                newDat[is.na(newDat)] <- thisDat[!sel,][is.na(newDat)]
+                exprs(expData[[i]])[!sel,p] <- newDat
+                warpedLandmarks[i, ] <- funs[[i]](landmarks[i,])
+                newRange[1] <- min(newRange[1], min(exprs(expData[[i]])[,p], na.rm=TRUE))
+                newRange[2] <- max(newRange[2], max(exprs(expData[[i]])[,p], na.rm=TRUE))
+            }
+            ## make sure that edge envents are set to the extreme values
+            ## of the warped data range and update the parameters slot
+            ## accordingly
+            for(i in seq_along(funs)){
+                minSel <- leftBoard[[i]]
+                maxSel <- rightBoard[[i]]
+                exprs(expData[[i]])[minSel,p] <- as.matrix(rep(newRange[1],
+                                                               sum(minSel, na.rm=TRUE)),
+                                                           ncol=1)
+                exprs(expData[[i]])[maxSel,p] <- as.matrix(rep(newRange[2],
+                                                               sum(maxSel, na.rm=TRUE)),
+                                                           ncol=1)
+                ip <- match(p, pData(parameters(expData[[i]]))$name)
+                tmp <- parameters(expData[[i]])
+                oldRanges <- unlist(range(expData[[i]],p))
+                pData(tmp)[ip, c("minRange", "maxRange")] <- c(min(oldRanges[1], newRange[1]),
+                                                               max(oldRanges[2], newRange[2]))
+                expData[[i]]@parameters <- tmp
+            }
+            lm[[p]] <- list(prior=landmarks, warped=warpedLandmarks,
+                            warpFun=funs, regions=regions, dists=dists,
+                            hasPeak=hasPeak)
         }
-        ## make sure that edge envents are set to the extreme values
-        ## of the warped data range and update the parameters slot
-        ## accordingly
-        for(i in seq_along(funs)){
-            minSel <- leftBoard[[i]]
-            maxSel <- rightBoard[[i]]
-            exprs(expData[[i]])[minSel,p] <- as.matrix(rep(newRange[1],
-                                                           sum(minSel, na.rm=TRUE)),
-                                                       ncol=1)
-            exprs(expData[[i]])[maxSel,p] <- as.matrix(rep(newRange[2],
-                                                           sum(maxSel, na.rm=TRUE)),
-                                                        ncol=1)
-            ip <- match(p, pData(parameters(expData[[i]]))$name)
-            tmp <- parameters(expData[[i]])
-            oldRanges <- unlist(range(expData[[i]],p))
-            pData(tmp)[ip, c("minRange", "maxRange")] <- c(min(oldRanges[1], newRange[1]),
-                                                           max(oldRanges[2], newRange[2]))
-            expData[[i]]@parameters <- tmp
-        }
-        lm[[p]] <- list(prior=landmarks, warped=warpedLandmarks,
-                        warpFun=funs)
     }
+    if(warpFuns)
+        return(wfuns)
     regSet <- as(expData, "flowSet")
     phenoData(regSet) <- phenoData(x)
     regSet <- regSet[sampleNames(x)]
@@ -158,3 +180,227 @@ warpSet <- function(x, stains, grouping=NULL, monwrd=TRUE, subsample=NULL,
     regSet
 }
 
+
+
+
+
+## Some QA pots for a normalized data set
+normQA <- function(data, morph=c("^fsc", "^ssc"), channels, ask=TRUE)
+{
+    if(! "warping" %in% names(attributes(data)))
+        stop("This flowSet has not been normalized.")
+    fsc <- grep(morph[1], colnames(data), ignore.case=TRUE)
+    if(length(fsc)) fsc <- colnames(data)[min(fsc)] else
+    stop("No parameter matching regular expression '", morph[1], "'.\n",
+         "Please provide a valid backgating channel.")
+    ssc <- grep(morph[2], colnames(data), ignore.case=TRUE)
+    if(length(ssc)) ssc <- colnames(data)[min(ssc)] else
+    stop("No parameter matching regular expression '", morph[2], "'\n",
+         "Please provide a valid backgating channel.")
+    ninfo <- attr(data, "warping")
+    wchans <- if(missing(channels)) names(ninfo) else channels
+    if(!all(wchans %in% names(ninfo)))
+        stop("No normalization information available for one or more channels.")
+
+    ## Plot the amount of warping for each landmark
+    layout(matrix(1:length(wchans), ncol=1))
+    for(p in wchans)
+    {
+        nc <- ncol(ninfo[[p]]$warped)
+        nr <- nrow(ninfo[[p]]$warped)
+        matplot(ninfo[[p]]$prior, type="n",
+                ylab=p, xlab="sample", ylim=range(data[[1]])[,p],
+                main=paste("Landmark Warping", p))
+        pch <- ifelse(ninfo[[p]]$hasPeak, 20, 4)
+        for(i in seq_len(nc))
+            points(ninfo[[p]]$prior[,i], pch=pch[,i], col=i+1)
+        m <- apply(ninfo[[p]]$warped, 2, mean, na.rm=TRUE)
+        abline(h=m, col=seq_len(nc)+1, lty="dotted")
+        diffs <- abs(ninfo[[p]]$prior - ninfo[[p]]$warped)
+        for(i in seq_len(nc))
+          segments(x0=seq_len(nr), x1=seq_len(nr), y0=ninfo[[p]]$prior[,i],
+                   y1=m[i], col=i+1, lty="dotted")
+    }
+    par(ask=ask)
+    on.exit(par(ask=FALSE))
+
+    ## Plot the confidence of the landmark registration step
+    layout(matrix(1:length(wchans), ncol=1))
+    for(p in wchans)
+    {
+        nc <- ncol(ninfo[[p]]$warped)
+        nr <- nrow(ninfo[[p]]$warped)
+        dists <- 1-ninfo[[p]]$dists
+        #dists[is.na(dists)] <- 1
+        pch <- ifelse(ninfo[[p]]$hasPeak, 1, 4)
+        plot(rep(seq_len(nc), each=nr), as.vector(dists),
+             ylim=c(min(0.5, min(dists, na.rm=TRUE)), 1.05),
+             xlim=c(0, nc+1), xlab="peaks", ylab="confidence", xaxt="n",
+             pch=pch,
+             main=paste("Landmark Registration", p))
+        axis(1, at=seq_len(nc), labels=TRUE)
+        box()
+        present <- apply(ninfo[[p]]$dists, 2, function(x) sum(!is.na(x)))
+        text(seq_len(nc), rep(1.05, nc), paste(present, nr, sep="/"),
+             cex=0.8)
+        
+    }
+
+    ## Plot the warping functions
+    layout(matrix(1:length(wchans), ncol=1))
+    for(p in wchans)
+    {
+       xvals <-  seq(range(data[[1]], p)[1,], range(data[[1]], p)[2,], len=50)
+       yvals <- sapply(ninfo[[p]]$warpFun, function(fun) fun(xvals))
+       matplot(yvals, xvals, type="l", xlab="warped", ylab="original",
+               main=paste("Warping Functions", p))
+    }
+
+    ## Plot the projection in the FCS vs. SSC space
+    alldat <- Subset(as(data, "flowFrame")[,c(fsc, ssc)],
+                     boundaryFilter(c(fsc, ssc)))
+    allM <- allV <- list()
+    layout(matrix(1:length(wchans), ncol=1))
+    for(p in wchans)
+    {
+        nc <- ncol(ninfo[[p]]$warped)
+        nr <- nrow(ninfo[[p]]$warped)
+        ## First create rectangleGates from the peak regions
+        regions <- ninfo[[p]]$regions
+        wfuns <- ninfo[[p]]$warpFun
+        datc <-  Subset(data, boundaryFilter(c(fsc, ssc)))
+        nfc <- filter(datc, norm2Filter(c(fsc, ssc)))
+        vcc <- sapply(nfc, function(x)
+                         prod(sqrt(eigen(filterDetails(x)[[1]]$cov)$values))*pi)
+        contour(alldat, xlab=fsc, ylab=ssc,
+                main=paste("Backgating Shape", p), col="transparent")
+        tmp <- sapply(regions, rowMeans)
+        expval <- signif(if(!is.null(dim(tmp))) rowMeans(tmp) else mean(tmp),3)
+        legend("topleft", pch=20, col=seq_len(nc)+1,
+               legend=sprintf("Peak %d (mean %s=%g)", seq_len(nc), p, expval),
+               cex=0.8, bty="n")
+        for(i in seq_len(nc))
+        {
+            g <- list()
+            for(j in names(regions))
+            {
+                g[[j]] <- rectangleGate(matrix(wfuns[[j]](regions[[j]][i,]), ncol=1,
+                                               dimnames=list(NULL, p)))
+            }
+            dat <- Subset(Subset(data, filterList(g)), boundaryFilter(c(fsc, ssc)))
+      
+            crgb <- col2rgb(i+1)
+            col <- rgb(crgb[1,], crgb[2,], crgb[3,], 255*0.12, maxColorValue=255)
+            colc <- rgb(crgb[1,], crgb[2,], crgb[3,], 255*0.2, maxColorValue=255)
+            nf <- filter(dat, norm2Filter(c(fsc, ssc)))
+            for(f in nf)
+                gpolygon(f, col=col, border=colc)
+            m <- fsApply(dat, function(x) apply(x[,c(fsc, ssc)], 2, mean, na.rm=TRUE),
+                         use.exprs=TRUE)
+            v <- fsApply(dat, function(x) apply(x[,c(fsc, ssc)], 2, sd, na.rm=TRUE),
+                         use.exprs=TRUE)
+            vc <- sapply(nf, function(x)
+                         prod(sqrt(eigen(filterDetails(x)[[1]]$cov)$values))*pi)
+            #segments(x0=m[,1]-v[,1], x1=m[,1]+v[,1], y0=m[,2], y1=m[,2], col=i,
+            #         lwd=1)
+            #segments(x0=m[,1], x1=m[,1], y0=m[,2]-v[,2], y1=m[,2]+v[,2], col=i,
+            #         lwd=1)
+            #points(m, col=i)
+            relV <- vc/vcc
+            allV[[p]][[i]] <- relV
+            allM[[p]][[i]] <- m
+        }
+    }
+
+    for(p in wchans)
+    {
+        nc <- ncol(ninfo[[p]]$warped)
+        nr <- nrow(ninfo[[p]]$warped)
+        contour(alldat, col="lightgray", xlab=fsc, ylab=ssc,
+                main=paste("Backgating Centroids", p))
+        regions <- ninfo[[p]]$regions
+        tmp <- sapply(regions, rowMeans)
+        expval <- signif(if(!is.null(dim(tmp))) rowMeans(tmp) else mean(tmp),3)
+        legend("topleft", pch=20, col=seq_len(nc)+1,
+               legend=sprintf("Peak %d (mean %s=%g)", seq_len(nc), p, expval),
+               cex=0.8, bty="n")
+        for(i in seq_len(nc))
+            points(allM[[p]][[i]], col=i+1)
+    }
+
+    ## Plot the relative ellipse volumes for the backgated channels
+    layout(matrix(1:length(wchans), ncol=1))
+    for(p in wchans)
+    {
+        nc <- ncol(ninfo[[p]]$warped)
+        nr <- nrow(ninfo[[p]]$warped)
+        plot(seq_len(nc), rep(0, nc),
+             ylim=c(0,max(5, max(unlist(allV[[p]])))), type="n",
+             xlim=c(0, nc+1), xlab="peaks", ylab="variation", xaxt="n",
+             pch=pch, col=rep(seq_len(nc), each=nr),
+             main=paste("Backgating Variation", p))
+        axis(1, at=seq_len(nc), labels=TRUE)
+        box()
+        for(i in seq_len(nc))
+            points(rep(i, nr), allV[[p]][[i]]) 
+    }
+    invisible()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## An optimized object size function that is able to deal with embedded
+## environments.
+## setGeneric("objectSize", function(x, ...) standardGeneric("objectSize"))
+## setMethod("objectSize", signature(x="ANY"), definition=function(x, ...)
+##       {
+##           realObjSize <- function(object, size=0, done=list())
+##           {
+##               ## recursively go though environments and record the total size
+##               ## We also pass on pointers to all the environments that have
+##               ## already been counted in order to avoid infinite loops or
+##               ## counting things multiple times.
+##               objSize <- size
+##               if(is.environment(object) && !any(sapply(done, identical, object)))
+##               {
+##                   for(i in ls(object))
+##                       objSize <- realObjSize(get(i,object),objSize,
+##                                              done=append(done, object))
+##               }
+##               else
+##               {
+##                   objSize <- object.size(object, ...)+size
+##               }
+##               ## check if any of the slots are evironments and pass through those
+##               slots <- slotNames(object)
+##               envs <- sapply(slots, function(s) is.environment(slot(object, s)))
+##               for(i in slots[envs])
+##                   objSize <- realObjSize(slot(object,i), objSize, done)
+##               return(objSize)
+##           }
+##           realObjSize(x)
+##       })

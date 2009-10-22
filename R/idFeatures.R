746316b4d789cfd816755c9fb57c96d535452c3d
c.wong <c.wong@bc3139a8-67e5-0310-9ffc-ced21a209358>
2009-10-22 00:52:43 +0000

    use divisive cluster method, diana, to cluster the potential features and reduce the outliers.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowStats@42558 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/idFeatures.R
+++ R/idFeatures.R
@@ -1,97 +1,203 @@
-## use backgeting to identify the features and define a set of reference
-## features which are needed by function gpaSet
-
-idFeatures <- function(bg, merge.peak.cluster=TRUE, thres.merge=1/5, plot=FALSE)
+## identify and labeling significant features using backgating return values and
+## divisive clustering method -- diana.
+idFeatures <- function(bg, nDim, thres.merge=0.2, weight.mfeature=FALSE,
+                           plot=FALSE)
 {
     ## 'bg' is the return value of function flowStats:::backGating
 
+    if (thres.merge > 1) {
+        thres.merge <- 0.2
+        message("idFeatures: thres.merge must between 0 to 1. Defaulting
+        to 0.2.\n")
+    }
+    
     regFeatures <- list()
     refFeatures <- list()
-    ch = split(bg, factor(bg$channel))
+    ch <- split(bg, factor(bg$channel))
+    lambda <- 0.1
+    
     ## identify features on the projection of each bg channels
     for (i in names(ch))
     { 
         perChannel <- ch[[i]]
-       	centInd <- which(perChannel$type=="centroid")
-    	center  <- perChannel[centInd, ]
-        ## cluster the centriod based on peak+population. For example,
-        ## CD8 has two peaks, one peak has 3 population and one has 1 population.
-        ## Then, CD8 has four cluster center
-        center  <- labelCentroid(center) # add a slot: center$cluster
+        center <- perChannel[perChannel$type=="centroid", ]
+        ## use diversive hierarchical clustering (diff from agglomerative)
+        center<-.useDivCluster(center, 2, thres.merge, h.cluster="diana") 
+
+        ## flag the outliers
+        center <- .filterOutlier(center, lambda, channel=i, keep.outlier=TRUE)
         
-        ## let reference features = median of cluster members
-        refFeatures[[i]] <- t(sapply(split(center, center$cluster),
-                                function(a) apply(a[, 1:2], 2, median)))
-                  
+        refFeatures[[i]] <- .calcReference(center)
+                                                       
         perSample = split(center, factor(center$sample))
+        
+        ## if cluster labeled as an outlier...
+        idx <- grep("outlier", refFeatures[[i]]$cluster)
+        if (length(idx) > 0)
+            refF <- refFeatures[[i]][-idx, ]
+        else
+            refF <- refFeatures[[i]]
 
-        ## classifyFeatures based upon the cluster labelling done by labelCluster
+        ## classify features for each sample
         regFeatures[[i]] <- 
-             lapply(perSample, classifyFeatures, refFeatures[[i]],
-                    by="distance")
+            lapply(perSample, .classifyFeatures,
+                   refF=refF, nDim=nDim)
 
-        #if (plot) .plotResults(perChannel, center)
     }
 
-    ## combine the members of regFeatures for each samples
-   
+    ## combine all registered features and group features for each sample
     combf <- list()
     for (m in levels(factor(bg$sample))) {
          combf[[m]] <- do.call(rbind, 
                      lapply(regFeatures, function(a, b) {a[[b]]}, m))
     }
+    ## plot the result
+    if (plot)
+        print(lattice::xyplot(FSC~SSC|sample, group=cluster,
+                              data=do.call(rbind,combf),
+                              main="feature labeling",
+                              auto.key=list(space="right")
+                              ))
+    ## combind all the 
+    combf <- .useDivCluster(do.call(rbind, combf),
+                            nDim=2, h.cluster="diana",
+                            thres.merge=thres.merge,
+                            naming.channel=FALSE)
+    ## plot the result
+    if (plot)
+        print(lattice::xyplot(FSC~SSC|sample, group=cluster,
+                              data=combf,
+                              main="feature labeling: using diana",
+                              auto.key=list(space="right")
+                              ))           
     
-    reference <- do.call(rbind, refFeatures)
-    rownames(reference) <- rownames(combf[[1]])
-    reference <- as.data.frame(reference)
+    ## reduce outlier
+    combf <- .filterOutlier(combf, lambda=lambda, keep.outlier=FALSE)
+    reference <- .calcReference(combf)
     reference$bogus <- rep(FALSE, nrow(reference))
-    reference$cluster <- rownames(combf[[1]])
-    combf <- lapply(combf, function(x)
-                    {x$cluster=rownames(x); x})
-    combf[["reference"]] <- reference
 
     
-    if (merge.peak.cluster) {
-        ## merge clusters and re-calculate the reference points
-        combf <- mergeCluster(combf, thres=thres.merge)
-        ## re-calculate the reference?
-    }
+    ## need to classify the cluster center again, pick the feature that is
+    ## closer to the reference
+    combf <-lapply(split(combf, factor(combf$sample)),
+              function(a, refF, nDim) {
+                 perCluster <- split(a, factor(a$cluster))
+                 reg <- lapply(perCluster, function(b) {
+                     ## take false if there is any
+                     if (sum(!b$bogus)>0)
+                         b <- b[!b$bogus, ]
+                     
+                     tmp <- rbind(refF[b$cluster[1], 1:nDim], b[, 1:nDim])
+                     minD <- b[which.min(as.numeric(dist(tmp))[1:nrow(b)]), ]
+                 })
+                 do.call(rbind, reg)
+              }, reference, nDim)
+    ## check each sample has the same amont of feature
+    ## combf <- .checkFeatureNum(combf, reference, nDim)
+    if (plot)
+        print(lattice::xyplot(FSC ~ SSC | sample, group=cluster,
+                        data=do.call(rbind, combf),
+                        auto.key=list(title="cluster",space="right"),
+                        main="reduced features"))
+        #print(lattice::xyplot(FSC ~ SSC | sample, group=bogus,
+        #                data=do.call(rbind, combf),
+        #                auto.key=list(title="Bogus",space="right"),
+        #                main="reduced features"))
+    combf[["reference"]] <- reference
+
+     return(combf)
+}
+
+#################################
+## filter out outlier. 
+#################################
+.filterOutlier <-function(center, lambda, channel=NULL, keep.outlier=FALSE) {
+    nclust <- sapply(split(center, factor(center$cluster)),
+                         function(x) nrow(x))
+    outlier_thres <- round(length(unique(center$sample)) * lambda)
+    outlier <- names(nclust)[nclust <= outlier_thres]
+
+    if (length(outlier) > 0) { ## if there are outliters
+        if (keep.outlier) {
+            center$cluster[center$cluster %in% outlier] <-
+                 paste(channel, "outlier", sep=".")
+        }
+        else
+            center <- center[!center$cluster %in% outlier, ]
+     }
+    return(center)
+}
+
+###########################
+## calculate the refenrence
+###########################
+.calcReference <- function(center, method="median")  {
+      ref <- do.call(rbind, lapply(split(center, center$cluster),
+                 function(a) {
+                    ref <- as.data.frame(t(apply(a[, 1:2], 2, median)))
+                    ref$cluster <- a$cluster[1]
+                    return(ref)
+                  }))
+}
+
+
+#################################################
+## use divisive clustering method: diana or diasy
+#################################################
+.useDivCluster <- function(center, nDim, naming.channel=TRUE,
+                           thres.merge=0.2, h.cluster="diana") {
+    ## sign (or re-sign the cluster label)
+    clustD  <- cluster::diana(center[, 1:nDim])
+    heightThres <- max(clustD$height)*thres.merge
+
+    if (naming.channel)
+        center$cluster <- paste(center$channel, 
+                                stats::cutree(as.hclust(clustD),
+                                              h=heightThres),
+                            sep=".")
+    else
+        center$cluster <- stats::cutree(as.hclust(clustD), h=heightThres)
     
-    return(combf)
+    return(center)
 }
 
 ###################
 ## classifyFeatures
 ###################
-classifyFeatures <- function(eachSample, refF, by="distance")
+.classifyFeatures <- function(eachSample, refF, nDim)
 {
   ## make the size of refF and column names consistant with refF 
   ## initialize parameter
-  reg <- data.frame(apply(refF, 2, function(x) rep(NA, length(x))),
-                    bogus=rep(FALSE, nrow(refF)))
 
+  reg <- data.frame(apply(refF, 2, function(x) rep(NA, length(x))),
+                    sample=rep(eachSample$sample[1], nrow(refF)),
+                    bogus=rep(TRUE, nrow(refF)))
+  reg$cluster <- refF$cluster
+  
   perClust <- split(eachSample, factor(eachSample$cluster))
-       for (j in 1:length(perClust))
-       {
-           lab <- as.numeric(perClust[[j]]$cluster)[1]
-	   if (nrow(perClust[[j]]) == 1) {
-       	       reg[lab, c("x", "y")] <- as.matrix(perClust[[j]][, c("x", "y") ])
-	   }
-	   else {
-	       tmp <- rbind(refF[lab, c("x", "y")],
-                     perClust[[j]][, c("x", "y")])
-               mindist <-
+  if (!identical(grep("outlier", names(perClust)), integer(0)))
+      perClust <- perClust[-grep("outlier", names(perClust))]
+
+  dim <- seq_len(nDim)
+  
+  for (j in 1:length(perClust)) {
+       lab <- perClust[[j]]$cluster[1]
+       if (nrow(perClust[[j]]) == 1) 
+            reg[reg$cluster==lab, dim] <- as.matrix(perClust[[j]][, dim])
+       else {
+           tmp <- rbind(refF[lab, dim], perClust[[j]][,dim])
+           mindist <-
                    which.min(as.numeric(dist(tmp))[1:nrow(perClust[[j]])])
-	       reg[lab, c("x","y")] <- as.matrix(
-	          perClust[[j]][mindist, c("x", "y") ])
-           }
+           reg[reg$cluster==lab, dim] <- as.matrix(perClust[[j]][mindist, dim])
        }
+       reg$bogus[reg$cluster==lab] <- FALSE
+   }
 
   ## 2. deal with NA peaks, give the refFeatures and cluster status as bogus
     naIdx <- which(is.na(reg[, 1]))
   
     if (!identical(naIdx, integer(0))) {
-       reg[naIdx, c("x", "y")] <- refF[naIdx, c("x", "y")]
+       reg[naIdx, dim] <- refF[naIdx, dim]
        reg[naIdx, "bogus"] <- TRUE
     }
   
@@ -122,10 +228,10 @@ classifyFeatures <- function(eachSample, refF, by="distance")
                 data=center, 
                 main=paste("centriod per peak", channel)))
     	                
-   print(xyplot(x ~ y|which,
-                do.call(make.groups, lapply(regFeatures[[i]], as.data.frame)),
-                as.table=TRUE, 
-                main=paste("registered features with backgating on", channel)))
+   #print(xyplot(x ~ y|which,
+   #             do.call(make.groups, lapply(regFeatures[[i]], as.data.frame)),
+   #             as.table=TRUE, 
+   #             main=paste("registered features with backgating on", channel)))
             
    #Features=regFeatures[[i]]
    #Features$reference=refFeatures[[i]][, 1:2]
@@ -139,129 +245,27 @@ classifyFeatures <- function(eachSample, refF, by="distance")
    #   	        main=paste("registered features with backgating on", channel)))
 }
 
-useCluster <- function(center) {
-  ## decide k by BIC score
-  BIC <- mclustBIC(center[, 1:2])
-  plot(BIC)
-  
-  #a <- kmeans(center[, 1:2], centers=2)
-  #a <- Mclust(center[, 1:2]) #a$classification
-  
-  center$label <- factor(a$cluster)
-  design <- model.matrix(~0+center$label)
-  colnames(design) <- paste("clust", seq(ncol(design)), sep="")
-  aa <- apply(design, 2, function(x, y) y[as.logical(x),], center[, 1:2])
-  x11();xyplot(x~y, data=do.call(make.groups, lapply(aa, as.data.frame)),
-         group=which, auto.key=list(space="right"))
 
-}
-
-###############################################
-## labelling the centroid by kmeans clustering
-###############################################
-labelCentroid <- function(center) {
-
- 
-    perPeak = split(center, factor(center$peak))
-    nclust <- 0
-    km <- NULL
-    for (i in 1:length(perPeak)) {
-        ## if there are more than one population -> no clustering, get median
-        if (length(unique(perPeak[[i]]$population))==1) {
-            medPop <- apply(perPeak[[i]][, 1:2], 2, median)
-            perPeak[[i]]$cluster <- 1 + nclust 
+###################################################
+## check if the number of features for
+## each sample is equal to that of reference
+## if the feature is missing, replace with a bogus
+###################################################
+.checkFeatureNum <- function(combf, reference, nDim) {
+    lapply(combf, function(x) {
+        if (nrow(x) != nrow(reference)) {
+           i <- nrow(x)
+           x[i+1,] <- x[i,]
+           x[i+1, 1:(nDim+1)] <-
+               reference[!(reference$cluster %in% x$cluster), 1:(nDim+1)]
+           x$bogus[i+1] = TRUE
+           x <- x[sort(x$cluster), ]
         }
-        else {
-            perPop <- split(perPeak[[i]], factor(perPeak[[i]]$population))
-            medPop <- lapply(perPop, function(a) apply(a[, 1:2], 2, median))
-            km <- kmeans(perPeak[[i]][, 1:2], do.call(rbind, medPop))
-            perPeak[[i]]$cluster <- km$cluster+nclust
-        }
-        nclust <- nclust + length(medPop) 
-    }
-
-    
-    mPeak <- do.call(rbind, perPeak)
-    
-    return(do.call(rbind, perPeak))
+        x
+    })
 }
 
-## merge cluster
-#mergeCluster <- mergeCluster(mPeak)
-#{
-#    perClust <- split(mPeak, factor(mPeak$cluster))
-#    md <- t(sapply(perClust, function(x) apply(x[, 1:2], 2, median)))
-#    dst <- dist(md, diag=TRUE, upper=TRUE)
-#    ## use Hierarchical cluster analysis to merge the equivalent clusters
-#    ## and give label accordingly
-#    hdst <- hclust(dst)
-#
-#}
 
-mergeCluster <- function(feats, thres=1/5)
-{
-    sampleF <- feats[-which(names(feats)=="reference")]
-    ref <- feats$reference
-    dst <- dist(ref[, 1:2])
-    hdst <- hclust(dst)
-    JMAX = 7
-    nTimes = 1
-    #plot(hdst)
-    ## while loop: continue if the ratio of the shortest and longest distance
-    ## is less than 1/15
-    while(nTimes <= JMAX | min(dst)/max(dst) < thres) {
-        mergeIdx <- which(rowMeans(sign(hdst$merge)) < 0)
-        dst <- as.matrix(dst)
-        
-        ## fix reference (ref) if needed
-        ## fix index (mergeIdx) at which sampleF needed to be altered
-        fixIdx <- NULL
-        for (i in 1:length(mergeIdx)) {
-            nclust <- -hdst$merge[mergeIdx[i], ]
-            dratio <- dst[nclust[1], nclust[2]] / max(dst)   
-            if (dratio < thres) ## replace by the mean values
-                ref[nclust[1], 1:2] <-
-                    (ref[nclust[1], 1:2]+ref[nclust[2], 1:2])/2
-            else
-                fixIdx=cbind(fixIdx, i)
-        }
-        if (!is.null(fixIdx)) mergeIdx <- mergeIdx[-fixIdx]
-        
-        ## if mergeIdx is empty, then the distances between the reference are
-        ## sufficient large. -> no need to continue the loop
-        if (identical(mergeIdx, integer(0))) break
-            
-        ## fix the sampleF: get the one that is closer to the reference features
-        sampleF <- lapply(sampleF, function(sF, ref, cidx) {
-                          ## make sure cidx is an matrix
-                          if (!is.matrix(cidx)) cidx <- matrix(cidx, nrow=1)
-                          
-                          for (j in 1:nrow(cidx)) {
-                            x <- cidx[j,]
-                            dst <- dist(rbind(ref[x[1],], sF[x, ])[, 1:2])
-                            sF[x[1], 1:2] <-
-                              sF[x[which.min(as.numeric(dst)[1:2])], 1:2]
-                          }
-                      return(sF)
-                      }, ref, -hdst$merge[mergeIdx, ])
-        ## delete hdst$merge[mergeIdx, 2] from ref and sampleF
-        del <- -hdst$merge[mergeIdx,2]
-        ref <- ref[-del, ]
-        sampleF <- lapply(sampleF, function(x, del) x[-del, ], del)
-
-        ## get the hierarchical cluster analysis and distance
-        dst <- dist(ref[, 1:2])
-        hdst <- hclust(dst)
-        nTimes <- nTimes + 1
-    }
-    sampleF[["reference"]] <- ref
-    return(sampleF)
-}
-
-is.allNegative <- function(x) {
-    mean(sign(x))<0
-
-}
 .plotFeatures <- function(combf, reference) {
 
    ## per sample

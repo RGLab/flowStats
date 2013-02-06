c697e92a73e556fbfd6595a11ddc4fb9a38a68db
Greg Finak <gfinak@fhcrc.org>
2013-02-06 12:24:14 -0800

    Change new.env to new.env(emptyenv()) ' '

--- R/normalizeGatingSet.R
+++ R/normalizeGatingSet.R
@@ -218,7 +218,8 @@ setMethod("normalize",c("GatingSet","missing"),function(data,x="missing",...){
 					 		sapply(sampleNames(result),function(s)ncdfFlow:::updateIndices(result,s,NA))
 					
 							#assign result to the Gating set data environment
-							lfile<-flowWorkspace:::getNcdf(x)@file
+#							lfile<-flowWorkspace:::getNcdf(x)@file
+                                                        lfile<-flowWorkspace:::ncFlowSet(x)@file
 							lapply(x,function(gh)assign("ncfs",result,graph:::nodeDataDefaults(gh@tree,"data")[["data"]]))		
 						}else{
 							browser()

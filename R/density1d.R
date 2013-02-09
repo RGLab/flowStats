11e52820649c1bc42f98d7dd311872550fc4ec75
m.jiang <m.jiang@bc3139a8-67e5-0310-9ffc-ced21a209358>
2013-02-09 00:43:27 +0000

    merge from github:master to local master copy from bioc devel
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowStats@73305 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/density1d.R
+++ R/density1d.R
@@ -1,13 +1,215 @@
+hubers1<-function (y, k = 1.5, mu, s, initmu = median(y), tol = 1e-06) 
+{
+	mmu <- missing(mu)
+	ms <- missing(s)
+	y <- y[!is.na(y)]
+	n <- length(y)
+	if (mmu) {
+		mu0 <- initmu
+		n1 <- n - 1
+	}
+	else {
+		mu0 <- mu
+		mu1 <- mu
+		n1 <- n
+	}
+	if (ms) {
+		s0 <- mad(y)
+		if (s0 == 0) 
+			return(list(mu = mu0, s = 0))
+	}
+	else {
+		s0 <- s
+		s1 <- s
+	}
+	th <- 2 * pnorm(k) - 1
+	beta <- th + k^2 * (1 - th) - 2 * k * dnorm(k)
+	for (i in 1:30) {
+		yy <- pmin(pmax(mu0 - k * s0, y), mu0 + k * s0)
+		if (mmu) 
+			mu1 <- sum(yy)/n
+		if (ms) {
+			ss <- sum((yy - mu1)^2)/n1
+			s1 <- sqrt(ss/beta)
+		}
+		if ((abs(mu0 - mu1) < tol * s0) && abs(s0 - s1) < tol * 
+				s0) 
+			break
+		mu0 <- mu1
+		s0 <- s1
+	}
+	list(mu = mu0, s = s0)
+}
+density1d_simple <- function(x, stain, alpha="min", sd=2, plot=FALSE, borderQuant=0.1,
+		absolute=TRUE, inBetween=FALSE, refLine=NULL,rare=FALSE,bwFac = 1.2
+		,sig=NULL
+		,peakNr=NULL
+		,cutoff=0.05
+		,valley="min"
+		, ...)
+{
+	
+	## some type checking first
+	flowCore:::checkClass(x, c("flowFrame", "flowSet"))
+	flowCore:::checkClass(stain, "character", 1)
+	if(!stain %in% colnames(x))
+		stop("'", stain,"' is not a valid parameter in this flowFrame")
+	
+	flowCore:::checkClass(alpha, c("character", "numeric"), 1)
+#	browser()
+	flowCore:::checkClass(sd, "numeric", 1)
+	flowCore:::checkClass(plot, "logical", 1)
+	flowCore:::checkClass(borderQuant, "numeric", 1)
+	flowCore:::checkClass(absolute, "logical", 1)
+	if (!is.null(refLine))
+		flowCore:::checkClass(refLine, "numeric", 1)
+	flowCore:::checkClass(inBetween, "logical", 1)
+	
+	## collapse to flowFrame
+	if(is(x, "flowSet"))
+		x <- as(x, "flowFrame")
+#   browser()
+
+	#get density curve peaks	
+	fres<-filter(x,curv1Filter(stain,bwFac=bwFac))
+	fres<-filterDetails(fres)
+	fhat<-fres[[1]]$fsObj$fhat
+	rawSig<-fres[[1]]$fsObj$x
+
+	#fitted curve points
+	curve<-data.frame(x=fhat[[1]][[1]],y=fhat$est)
+#	browser()
+	curve_filtered<-subset(curve,y>=cutoff)
+	#peak boudaries
+	boundaries<-fres[[1]]$boundaries
+	#filter peaks as well
+	isNoise<-unlist(lapply(boundaries,function(curbn){
+#				browser()
+				subCurve<-subset(curve,x>=curbn[1]&x<=curbn[2])
+				peak_mode<-subCurve[which.max(subCurve$y),]	
+				peak_mode$y<cutoff
+			}))
+	boundaries<-boundaries[!isNoise]
+	nPeaks<-length(boundaries)
+#	plot(curve)
+#	plot(curve_filtered)
+#	browser()
+	if(rare||nPeaks==1)
+	{
+		if(nPeaks==1)
+		{
+			peak_mode<-curve_filtered[which.max(curve_filtered$y),]
+			#duplicate the lhs of the peak and try to get correct estimation of mean and sd
+			#without the distortion of another peak
+			if(!is.null(sig))
+				if(sig=="L")
+				{
+					halfS<-rawSig[rawSig<=peak_mode[,"x"]]
+#					halfCurve<-curve[curve$x<=peak_mode[,"x"],]
+					
+				}else if(sig=="R")
+				{
+#					halfCurve<-curve[curve$x>=peak_mode[,"x"],]
+					halfS<-rawSig[rawSig>=peak_mode[,"x"]]
+				}else
+					stop("invalid value for 'sig' !")
+#			plot(halfS)
+		}else  
+		{
+			##pick LHS of far left peak or RHS of far right peak 
+			#when there are more than 1 peak
+			if(!is.null(sig))
+				if(sig=="L")
+				{
+					lpeak_bnd<-boundaries[[1]]
+					subCurve<-subset(curve,x>=lpeak_bnd[1]&x<=lpeak_bnd[2])
+					peak_mode<-subCurve[which.max(subCurve$y),]	
+#					halfCurve<-curve[curve$x<=peak_mode$x,]
+					halfS<-rawSig[rawSig<=peak_mode[,"x"]]
+				}else if(sig=="R")
+				{
+					rpeak_bnd<-boundaries[[nPeaks]]
+					subCurve<-subset(curve,x>=rpeak_bnd[1]&x<=rpeak_bnd[2])
+					peak_mode<-subCurve[which.max(subCurve$y),]	
+#					halfCurve<-curve[curve$x>=peak_mode$x,]
+					halfS<-rawSig[rawSig>=peak_mode[,"x"]]
+			
+				}else
+					stop("invalid value for 'sig' !")
+#			plot(halfS)
+			
+		}
+		if(is.null(sig))
+		{
+			#use entire signal to estimate mode and sd
+			est <- hubers(curve$x)
+		}else
+		{
+#			browser()
+#			sd1<-sqrt(mean((halfCurve$x-peak_mode[,"x"])^2*halfCurve$y))*2
+#			sd1<-sqrt(mean((halfS-peak_mode[,"x"])^2))
+			sd1<-median(abs(halfS-peak_mode[,"x"]))
+#			sd1<-hubers(halfS)$s
+			est<-list(mu=peak_mode[,"x"],s=sd1)
+			
+			
+			
+		}
+		
+		loc <- est$mu + sd * est$s
+			
+	}else
+	{
+		
+		#get valleys
+		valleys<-do.call(rbind
+				,lapply(1:(nPeaks-1),function(i){
+							#		browser()
+							#get valley boundaries
+							valleyLbound<-max(boundaries[[i]])
+							valleyRbound<-min(boundaries[[i+1]])
+							
+							#
+							subCurve<-subset(curve,x>=valleyLbound&x<=valleyRbound)
+							#		plot(subCurve)
+							subCurve[which.min(subCurve$y),]
+							
+						})
+				)   	
+		if(valley=="min")#pick lowest valley
+			loc<-valleys[which.min(valleys$y),"x"]
+		else
+			loc<-valleys[valley,"x"]
+			
+			
+	}
+	## create output if needed
+#	browser()
+	if(plot){
+		plot(curve,type="l",main=paste("breakpoint for parameter", stain), cex.main=1, ...)
+		abline(v = loc, col = 2, lwd = 2)
+		
+	}
+#	browser()
+	return(loc)
+}
+
 ## Find most likely separator between peaks in 1D
 density1d <- function(x, stain, alpha="min", sd=2, plot=FALSE, borderQuant=0.1,
-                      absolute=TRUE, inBetween=FALSE, refLine=NULL,rare=FALSE,bwFac = 1.2, ...)
+                      absolute=TRUE, inBetween=FALSE, refLine=NULL,rare=FALSE,bwFac = 1.2
+			  		,sig=NULL
+					,peakNr=NULL
+			  		, ...)
 {
+	
     ## some type checking first
     flowCore:::checkClass(x, c("flowFrame", "flowSet"))
     flowCore:::checkClass(stain, "character", 1)
     if(!stain %in% colnames(x))
         stop("'", stain,"' is not a valid parameter in this flowFrame")
+	
     flowCore:::checkClass(alpha, c("character", "numeric"), 1)
+#	browser()
     flowCore:::checkClass(sd, "numeric", 1)
     flowCore:::checkClass(plot, "logical", 1)
     flowCore:::checkClass(borderQuant, "numeric", 1)
@@ -19,6 +221,7 @@ density1d <- function(x, stain, alpha="min", sd=2, plot=FALSE, borderQuant=0.1,
     ## collapse to flowFrame
     if(is(x, "flowSet"))
        x <- as(x, "flowFrame")
+#   browser()
     ## clip to data range (if needed) and compute density peaks
     if(absolute){
         vrange <- range(x, na.rm=TRUE)[, stain]
@@ -32,39 +235,84 @@ density1d <- function(x, stain, alpha="min", sd=2, plot=FALSE, borderQuant=0.1,
     inc <- diff(vrange)/1e5
     exprf <- char2ExpressionFilter(sprintf("`%s` > %s & `%s` < %s", stain,
                                            vrange[1]+inc, stain, vrange[2]-inc))
+						   
     fres <- filter(tmp <- Subset(x, exprf), curv1Filter(stain,bwFac=bwFac))
     bnds <- flowStats:::curvPeaks(fres, exprs(tmp)[, stain], borderQuant=borderQuant)
+	##when peakNr is present,drop the less significant peaks by their heights
+	
+	if(!is.null(peakNr))
+	{
+		if(peakNr<nrow(bnds$peaks))
+		{
+			bigPks<-order(bnds$peaks[,"y"],decreasing=T)[1:peakNr]
+			bnds$peaks<-bnds$peaks[bigPks,]
+			bnds$regions<-bnds$regions[bigPks,]
+			bnds$densFuns<-bnds$densFuns[bigPks]
+			
+		}
+	}
+	
     ## define 'positive' and 'negative' peaks by proximity to anchors
     anchors <- quantile(as.numeric(vrange), c(0.25, 0.5))
     anc <- abs(sapply(anchors, "-", bnds$peaks[, "x", drop=FALSE]))
     dens <- density(exprs(tmp)[, stain])
     ## only one peak: use robust estimation of mode and variance for boundary
-    est <- hubers(exprs(tmp[, stain]))
+	signals<-exprs(tmp[, stain])
+		
+#		browser()
     if(is.null(nrow(anc)))
     {
+		if(!is.null(sig))
+		{
+			if(sig=="L")
+			{
+				halfS<-signals[signals<=bnds$peak[,"x"]]
+				
+			}
+			if(sig=="R")
+			{
+				halfS<-signals[signals>=bnds$peak[,"x"]]
+			}
+			
+			varS<-sqrt(mean((halfS-bnds$peak[,"x"])^2))
+			
+			est<-list(mu=bnds$peak[,"x"],s=varS)
+		}else
+		{
+			est <- hubers(signals)
+			
+		}
+		
         class <- which.min(abs(est$mu - anchors))
-    }
-    else if(nrow(anc)==2 && inBetween)
-    {
-        left <- bnds$regions[1,"right"]
-        right <-  bnds$regions[2,"left"]
-        class <- 1:2
-    }
-    else
-    {
-        ## multiple peaks: classify as pos or neg and set boundary between
-        class <- apply(anc,1, which.min)
-        left <- ifelse(sum(class==1)>0, max(bnds$regions[class==1, "right"]),
-                       min(bnds$regions[class==2, "left"]))
-        right <- ifelse(sum(class==2)>0, min(bnds$regions[class==2, "left"]),
-                        max(bnds$regions[class==1, "right"]))
-    }
-
+    }else 
+	{
+		est <- hubers(signals)
+		if(nrow(anc)==2 && inBetween)
+	    {
+	        left <- bnds$regions[1,"right"]
+	        right <-  bnds$regions[2,"left"]
+	        class <- 1:2
+	    }
+	    else
+	    {
+	        ## multiple peaks: classify as pos or neg and set boundary between
+	        class <- apply(anc,1, which.min)
+	        left <- ifelse(sum(class==1)>0, max(bnds$regions[class==1, "right"]),
+	                       min(bnds$regions[class==2, "left"]))
+	        right <- ifelse(sum(class==2)>0, min(bnds$regions[class==2, "left"]),
+	                        max(bnds$regions[class==1, "right"]))
+	    }
+	}
 	
 	
+		
 	##if rare==true then assume there is only one major peak and the rare population is at the right tail
-    if(rare||all(class==1))
-        bound <- est$mu + sd * est$s
+#    browser()
+	if(rare||all(class==1))
+	{
+		bound <- est$mu + sd * est$s
+		
+	}
     else if(all(class==2))
         bound <- est$mu - sd * est$s
     else{ 
@@ -73,11 +321,19 @@ density1d <- function(x, stain, alpha="min", sd=2, plot=FALSE, borderQuant=0.1,
                 sel <- (dens$x > left & dens$x <= right)
                 bound <- dens$x[sel][which.min(dens$y[sel])]
             }else
-            stop("unknown value for alpha")
+			{
+				alpha<-as.numeric(alpha)
+				if(is.na(alpha))
+					stop("unknown value for alpha")
+				else
+					bound <- (1-alpha)*left + alpha*right
+			}
+            
         }else
         bound <- (1-alpha)*left + alpha*right    
     }
     ## create output if needed
+#	browser()
     if(plot){
         plot(dens, main=paste("breakpoint for parameter", stain), cex.main=1, ...)
         regs <- bnds$regions
@@ -99,9 +355,13 @@ density1d <- function(x, stain, alpha="min", sd=2, plot=FALSE, borderQuant=0.1,
 ## A wrapper around density1D directly creating a range gate.
 rangeGate <- function(x, stain, alpha="min", sd=2, plot=FALSE, borderQuant=0.1,
                      absolute=TRUE, filterId="defaultRectangleGate",
-                     positive=TRUE, refLine=NULL, ...)
+                     positive=TRUE, refLine=NULL,simple=FALSE, ...)
 {
-    loc <- density1d(x=x, stain=stain, alpha=alpha, sd=sd, plot=plot,
+	if(simple)
+		loc <- density1d_simple(x=x, stain=stain, alpha=alpha, sd=sd, plot=plot,
+				borderQuant=borderQuant, absolute=absolute, refLine=refLine, ...)
+	else	
+    	loc <- density1d(x=x, stain=stain, alpha=alpha, sd=sd, plot=plot,
                      borderQuant=borderQuant, absolute=absolute, refLine=refLine, ...) 
     bounds <- if(positive) list(c(loc, Inf)) else list(c(-Inf, loc))
     names(bounds) <- stain
--- R/quadrantGate.R
+++ R/quadrantGate.R
@@ -1,7 +1,10 @@
 ## Do quad gating of a flowSet or flowFrame
 quadrantGate <- function(x, stains, alpha = c("min", "min"), sd=c(2,2),
                          plot=FALSE, filterId="defaultQuadGate",
-                         refLine.1=NULL, refLine.2=NULL, ...)
+                         refLine.1=NULL, refLine.2=NULL
+				 		,rare=c(FALSE,FALSE)
+				 		,sig=c(NULL,NULL)
+						,...)
 {
     ## some type checking first
     flowCore:::checkClass(x, c("flowFrame", "flowSet"))
@@ -13,6 +16,9 @@ quadrantGate <- function(x, stains, alpha = c("min", "min"), sd=c(2,2),
     alpha <- rep(alpha, length = 2)[1:2]
     flowCore:::checkClass(alpha, c("character", "numeric"), 2)
     sd <- rep(sd, length=2)[1:2]
+	rare <- rep(rare, length=2)[1:2]
+	sig <- rep(sig, length=2)[1:2]
+	
     flowCore:::checkClass(sd, "numeric", 2)
     
     if (!is.null(refLine.1))
@@ -32,14 +38,16 @@ quadrantGate <- function(x, stains, alpha = c("min", "min"), sd=c(2,2),
     if(is(x, "flowSet"))
         x <- as(x, "flowFrame")
     boundX <- density1d(x, stains[1], alpha = alpha[1], plot, sd=sd[1],
-                        refLine = refLine.1, ...)
+                        refLine = refLine.1,rare=rare[1],sig=sig[1], ...)
     boundY <- density1d(x, stains[2], alpha = alpha[2], plot, sd=sd[2],
-                        refLine = refLine.2, ...)
+                        refLine = refLine.2,rare=rare[2], sig=sig[2],...)
+				
+
     ## add final plot if needed
     if(plot){
         ## FIXME: Ugly hack foe name space issue
         plot <- selectMethod("plot", c("flowFrame", "character"))
-        plot(x, stains, main=paste("Quad-gate for parameters\n",
+        plot(x, stains, main=paste(basename(description(x)$FIL),"\n Quad-gate for parameters\n",
                                    paste(stains, collapse=" and ")), cex.main=1)
         abline(h = boundY, v = boundX)
     }

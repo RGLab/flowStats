4b9e4f24a1cf7c1ed9c1136b985b108f159a623b
mikejiang <mike@ozette.ai>
2023-07-18 13:00:52 -0700

    update spillover

--- tests/testthat/test-spillover.R
+++ tests/testthat/test-spillover.R
@@ -20,6 +20,7 @@ test_that("spillover: Match columns using regexpr", {
   comp <- spillover(controls, unstained = "UNSTAINED", fsc="FSC-H",
                     ssc="SSC-H", patt = "-H", stain_match="regexpr",
                     useNormFilt = FALSE)
+  
   expect_equal(colnames(comp), colnames(comp_ref))
   expect_equal(rownames(comp), rownames(comp_ref))
   expect_equivalent(comp, comp_ref, tolerance=3e-08)
@@ -29,13 +30,19 @@ test_that("spillover: Match columns using ordered", {
   ref_file <- system.file("extdata", "compdata", "compref2", package="flowCore")
   comp_ref <- as.matrix(read.table(ref_file, check.names = FALSE))
   # The spillover matrix here should (appropriately) be incorrect bc the rows are
-  # not in the order of the columns.
-  comp <- spillover(controls, unstained = "UNSTAINED", fsc="FSC-H",
+  # not in the order of the columns. In fact we expect a warning as we'll check now for
+  # multiple off diagonal entries greater than 1.
+  comp<-expect_warning(spillover(controls, unstained = "UNSTAINED", fsc="FSC-H",
                     ssc="SSC-H", patt = "-H", stain_match="ordered",
-                    useNormFilt = FALSE)
-  expect_equal(colnames(comp), colnames(comp_ref))
-  expect_equal(rownames(comp), rownames(comp_ref))
-  expect_equivalent(comp, comp_ref, tolerance=3e-08)
+                    useNormFilt = FALSE))
+  comp_ref <-
+structure(c(1, 0.0077220477483574751, 0.23582570418697035, 0.065683952675143667, 
+0.24202227763070464, 1, 0.29890069124793922, 0.76478483921335927, 
+0.032083705820198934, 0.14078823194024204, 1, 4.3555171757803084, 
+0.0011278155536909003, 0.0026326890940635483, 310.69275900646016, 
+1), .Dim = c(4L, 4L), .Dimnames = list(c("FL1-H", "FL2-H", "FL3-H", 
+"FL4-H"), c("FL1-H", "FL2-H", "FL3-H", "FL4-H")))
+expect_equivalent(comp,comp_ref)
 })
 
 test_that("spillover: Match columns using intensity", {
@@ -98,7 +105,9 @@ test_that("spillover_ng: Using preconstructed flowSet with filenames as sample n
 
 test_that("spillover_ng: Using preconstructed flowSet with channels in new order", {
   match <- read.csv(matchfile_path, colClasses = "character")
+  ref_file <- system.file("extdata", "compdata", "compref4", package="flowCore")
   fs <- read.flowSet(files=match$filename, path=control_path)
+  comp_ref <- as.matrix(read.table(ref_file, check.names = FALSE))
 
   # Scramble the channel order
   fs <- fs[,c("FSC-H", "FL4-H", "FL3-H", "FL1-A", "FL1-H", "FL2-H", "SSC-H")]
